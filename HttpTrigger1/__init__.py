048aa39c259a986c993f7b6bc520eb955313fae3
Riaz <707477885@qq.com>
2021-05-19 07:28:02 +0000

    add hello

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -6,7 +6,7 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    logging.info('Hello')
     name = req.params.get('name')
     if not name:
         try:
